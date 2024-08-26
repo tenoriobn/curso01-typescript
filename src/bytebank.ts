1f51265344819ae0e0c8a053b55c424dd65d035c
Bruno Tenorio <tenoriobn@gmail.com>
2024-08-26 15:45:39 -0300

    Definindo tipos

--- src/bytebank.ts
+++ src/bytebank.ts
@@ -1,5 +1,7 @@
 let saldo = 3000;
 
+alert('Outra frase aqui');
+
 const elementoSaldo = document.querySelector(".saldo-valor .valor") as HTMLElement;
 if (elementoSaldo !== null) {
   elementoSaldo.textContent = saldo.toString();
