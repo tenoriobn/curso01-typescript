c1b6be70b71dfc8d0215eb07480f02996e44d28c
Bruno Tenório <tenoriobn@gmail.com>
2024-09-06 13:10:31 -0300

    Modularizando visualização Data e Saldo

--- dist/js/components/saldo-component.js
+++ dist/js/components/saldo-component.js
@@ -1,11 +1,6 @@
-import { formatarData, formatarMoeda } from "../utils/formatters.js";
-import { FormatoData } from "../types/FormatoData.js";
+import { formatarMoeda } from "../utils/formatters.js";
 import Conta from "../types/Conta.js";
 const elementoSaldo = document.querySelector(".saldo-valor .valor");
-const elementoDataAcesso = document.querySelector(".block-saldo time");
-if (elementoDataAcesso !== null) {
-    elementoDataAcesso.textContent = formatarData(Conta.getDataAcesso(), FormatoData.DIA_SEMANA_DIA_MES_ANO);
-}
 renderizarSaldo();
 function renderizarSaldo() {
     if (elementoSaldo != null) {
